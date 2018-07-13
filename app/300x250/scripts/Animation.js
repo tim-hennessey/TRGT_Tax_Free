efd9349000eef91400c3948bfb3c9ce82ae197d6
tim-hennessey <tim.hennessey@deutschinc.com>
2018-07-12 18:49:36 -0700

    Creative Update

--- app/300x250/scripts/Animation.js
+++ app/300x250/scripts/Animation.js
@@ -1,27 +1,48 @@
-var app = app || {}; 
-
+var app = app || {};
 
 app.Animation = (function () {
 
-	var title;
-	var caption;
+    var t = TweenMax;
+    var tl1 = new TimelineMax();
+    var txt1 = document.getElementById('txt1');
+
+    var erasers1 = document.getElementById('erasers1');
+    var pencils = document.getElementById('pencils');
+    var sharpeners = document.getElementById('sharpeners');
+    var crayons = document.getElementById('crayons');
+    var erasers = document.getElementById('erasers');
+
+    var cta = document.getElementById('cta');
+    var cta_txt = document.getElementById('cta_txt');
+    var cta_txt_ovr = document.getElementById('cta_txt_ovr');
+    var buttonExit = document.getElementById('button-exit');
 
 	// --------------------------------------------------------------------------------------
-	// set default properties
 	function initialize() {
-		title = document.getElementById('title');
-		caption = document.getElementById('caption');
+		t.set(banner, {opacity:1});
 
-		TweenLite.set(title, {scale:0});
-		TweenLite.set(caption, {scale:0});
+        buttonExit.addEventListener('mouseover', function () {
+            TweenMax.to(cta, .25, {backgroundColor: "#000000"});
+            TweenMax.to(cta_txt_ovr, .25, {opacity:1});
+            TweenMax.to(cta_txt, .25, {opacity:0});
+        });
+
+        buttonExit.addEventListener('mouseout', function () {
+            TweenMax.to(cta, .25, {backgroundColor: "#ffffff"});
+            TweenMax.to(cta_txt_ovr, .25, {opacity:0});
+            TweenMax.to(cta_txt, .25, {opacity:1});
+        });
+
 	}
-
 	// --------------------------------------------------------------------------------------
-	// Starts the animation
 	function start() {
-		TweenLite.to(title, 1, {scale:1, ease:Cubic.easeInOut});
-		TweenLite.to(caption, 1, {scale:1, ease:Cubic.easeInOut});
-	} 
+        tl1.from(erasers1, 1, {x: -205, y:250, ease: Cubic.easeOut})
+            .from(pencils, 1, {x: -205, y:250, ease: Cubic.easeOut},"-=.9")
+            .from(sharpeners, 1, {x: -205, y:250, ease: Cubic.easeOut},"-=.75")
+            .from(crayons, 1, {x: -205, y:250, ease: Cubic.easeOut},"-=1")
+            .from(erasers2, 1, {x: -205, y:250, ease: Cubic.easeOut},"-=.75")
+            .from(txt1, 1, {y: -75, ease: Cubic.easeInOut})
+	}
 
 	// --------------------------------------------------------------------------------------
 	// Stops the animation
@@ -33,7 +54,7 @@ app.Animation = (function () {
 	// Publicly accessible methods and properties
 	return {
 		initialize:initialize,
-		start:start, 
+		start:start,
 		stop:stop
 	}
 
